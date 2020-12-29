e093689d5d1728546c444f4f4933822fdc7376aa
ErenOzkacar <ErenOzkacar@icloud.com>
2020-12-29 07:04:39 +0100

    Initial Commit

---
+++ Random Projects/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Random Projects
+//
+//  Created by Eren Özkacar on 29.12.20.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
